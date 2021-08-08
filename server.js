cf94a71783ebfbdd01703128330ca6cc46db25bc
KhaledAlqrainy <khaledalqrainy@gmail.com>
2021-08-08 04:20:31 +0300

    solve some lab08

--- server.js
+++ server.js
@@ -8,6 +8,17 @@ server.use(cors());
 require('dotenv').config();
 const PORT = process.env.PORT;
 
+class Movie {
+    constructor(movie) {
+        this.title = movie.title
+        this.overview = movie.overview
+        this.vote_average = movie.vote_average
+        this.vote_count = movie.vote_count
+        this.poster_path = `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+        this.popularity = movie.popularity
+        this.release_date = movie.release_date
+    }
+}
 
 class Forecast {
     constructor (day) {
@@ -50,6 +61,25 @@ server.get('./weather', (req,res) =>{
     
 
 });
+
+server.get('/movies', getMovies)
+
+async function getMovies(req, res) {
+    let returnArr = [];
+    let { cityName } = req.query;
+    const URL = `https://api.themoviedb.org/3/search/movie?api_key=mSjXydZRVkSuJXcfMURFNgfcSKMmSEANpAKgBdcJgvc&query=${cityName}`;
+
+    try {
+        let axiosMovies = await axios.get(URL)
+        console.log(axiosMovies.data.results);
+        axiosMovies.data.results.map(item => returnArr.push(new Movie(item)));
+        res.status(200).send(returnArr);
+    }
+    catch (error) {
+        res.status(400)
+        if (error.status) res.status(error.status).send(error.message)
+    }
+}
 
 // console.log(getweather);
 
