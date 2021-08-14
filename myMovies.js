8c55c7c832a434eef345cf73a5b365f6cc8a9694
KhaledAlqrainy <khaledalqrainy@gmail.com>
2021-08-14 06:10:23 +0300

    final touch

--- myMovies.js
+++ myMovies.js
@@ -3,10 +3,21 @@
 const axios=require('axios')
 
 
+class Movie {
+    constructor(movie) {
+        this.title = movie.title
+        this.overview = movie.overview
+        this.vote_average = movie.vote_average
+        this.vote_count = movie.vote_count
+        this.popularity = movie.popularity
+        this.release_date = movie.release_date
+    }
+}
+
 async function getMovies(req, res) {
     let returnArr = [];
     let { cityName } = req.query;
-    const URL = `https://api.themoviedb.org/3/search/movie?api_key=mSjXydZRVkSuJXcfMURFNgfcSKMmSEANpAKgBdcJgvc&query=${cityName}`;
+    const URL = `https://api.themoviedb.org/3/search/movie?api_key=${process.env.MOVIE_API_KEY}&query=${cityName}`;
 
     try {
         let axiosMovies = await axios.get(URL)
@@ -21,17 +32,7 @@ async function getMovies(req, res) {
 }
 
 
-class Movie {
-    constructor(movie) {
-        this.title = movie.title
-        this.overview = movie.overview
-        this.vote_average = movie.vote_average
-        this.vote_count = movie.vote_count
-        this.poster_path = `https://image.tmdb.org/t/p/w500${movie.poster_path}`
-        this.popularity = movie.popularity
-        this.release_date = movie.release_date
-    }
-}
+
 
 module.exports = getMovies;
 
--- myWeather.js
+++ myWeather.js
@@ -2,39 +2,27 @@
 const axios = require('axios')
 
 
+class Forecast {
+    constructor(day) {
+        this.date = day.datetime;
+        this.description = day.weather.description;
+    }
+}
 
 async function getWeather(req, res) {
-    try{
 
-        const lon = req.query.lon;
-        const lat = req.query.lat;
-        const city_name = req.query.city_name;
-    
-        let getweather = weatherdata.find(i => i.city_name.toLowerCase() === city_name.toLowerCase() || (i.lat === lat && i.lon === lon) 
-        )
-        
-        if (!getweather) res.status(404).send('no data found');
-
-    
-        let myweather = getweather.data.map(i => new Forecast(i));
-    
-        res.send(myweather);
+    try {
+        let { searchQuery, lat, lon } = req.query;
+        if (!searchQuery) searchQuery = "doesn't match any city"
+        const URL = `https://api.weatherbit.io/v2.0//forecast/daily?key=${process.env.WEATHER_API_KEY}&lat=${lat}&lon=${lon}`
+        let axiosWeather = await axios.get(URL)
+        let forecastArr = axiosWeather.data.data.map(info => new Forecast(info));
+        res.send(forecastArr);
     }
-
-    catch {}
-
-    
-    
-
-}
-
-class Forecast {
-    constructor (day) {
-        this.datetime = day.datetime;
-        this.description = day.weather.description
+    catch (e) {
+        res.status(e.status).send({ status: e.status, description: `Error ${e.message}` });
     }
+};
 
-    
-}
 
 module.exports=getWeather;
