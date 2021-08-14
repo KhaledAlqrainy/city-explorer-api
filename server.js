6a75a7eb5ad01033a99f587bba5c15ac6e0a2a57
KhaledAlqrainy <khaledalqrainy@gmail.com>
2021-08-14 04:14:46 +0300

    try solve lab08

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
-const express = require ('express');
+
+'use strict'
+
+const express = require('express');
 const server = express();
 
-const weatherdata = require('./weather.json')
-const cors = require('cors'); // connect between backend and front end.
+require('dotenv').config();
+const PORT = process.env.PORT
+
+const cors = require('cors');
 server.use(cors());
 
-require('dotenv').config();
-const PORT = process.env.PORT;
+const axios = require('axios');
 
+class Forecast {
+    constructor(day) {
+        this.date = day.datetime;
+        this.description = day.weather.description;
+    }
+}
 class Movie {
     constructor(movie) {
         this.title = movie.title
@@ -20,54 +30,31 @@ class Movie {
     }
 }
 
-class Forecast {
-    constructor (day) {
-        this.datetime = day.datetime;
-        this.description = day.weather.description
-    }
+server.get('/', (req, res) => { res.send('Hello from main route'); });
 
-    
-}
-
-server.get('/', (req,res)=>{
-    res.send('this is my main route')
-} )
-
-
-// http://localhost:3001/weather?lon=31.95&lat=35.91&city_name=Amman
-// http://localhost:3001/weather?
-server.get('./weather', (req,res) =>{
-
-    try{
-
-        const lon = req.query.lon;
-        const lat = req.query.lat;
-        const city_name = req.query.city_name;
-    
-        let getweather = weatherdata.find(i => i.city_name.toLowerCase() === city_name.toLowerCase() || (i.lat === lat && i.lon === lon) 
-        )
-        
-        if (!getweather) res.status(404).send('no data found');
-
-    
-        let myweather = getweather.data.map(i => new Forecast(i));
-    
-        res.send(myweather);
-    }
-
-    catch {}
-
-
-    
-
-});
-
+server.get('/weather', getWeather)
 server.get('/movies', getMovies)
+
+async function getWeather(req, res) {
+
+    try {
+        let { searchQuery, lat, lon } = req.query;
+        if (!searchQuery) searchQuery = "doesn't match any city"
+        const URL = `https://api.weatherbit.io/v2.0//forecast/daily?key=b1e043fdf8e9499f970885461a077df5&lat=${lat}&lon=${lon}`
+        let axiosWeather = await axios.get(URL)
+        let forecastArr = axiosWeather.data.data.map(info => new Forecast(info));
+        // console.log(forecastArr);
+        res.send(forecastArr);
+    }
+    catch (e) {
+        res.status(e.status).send({ status: e.status, description: `Unable to Process Request.  Query String Correct? ${e.message}` });
+    }
+};
 
 async function getMovies(req, res) {
     let returnArr = [];
     let { cityName } = req.query;
-    const URL = `https://api.themoviedb.org/3/search/movie?api_key=mSjXydZRVkSuJXcfMURFNgfcSKMmSEANpAKgBdcJgvc&query=${cityName}`;
+    const URL = `https://api.themoviedb.org/3/movie/550?api_key=749d0a27c7aa89303b2fecaf56391905&query=${cityName}`;
 
     try {
         let axiosMovies = await axios.get(URL)
@@ -80,14 +67,4 @@ async function getMovies(req, res) {
         if (error.status) res.status(error.status).send(error.message)
     }
 }
-
-// console.log(getweather);
-
-server.get('*',(req,res) => {
-    res.status(404).send('route not found');
-})
-
-server.listen (PORT, () => {
-    console.log(`Im listening to ${PORT}`)
-})
-
+server.listen(PORT, () => console.log(`listening on ${PORT}`));
